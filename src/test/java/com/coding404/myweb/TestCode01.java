af2404d24ad72a435aad871436c5fc1f4c846648
CUTLETER <hellooohelloo@gmail.com>
2024-08-12 18:03:39 +0900

    0812

---
+++ src/test/java/com/coding404/myweb/TestCode01.java
@@ -0,0 +1,33 @@
+package com.coding404.myweb;
+
+import com.coding404.myweb.command.ProductVO;
+import com.coding404.myweb.product.ProductMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestCode01 {
+//    MySQL에 반복문 돌려서 대량의 데이터 삽입
+//    @Autowired
+//    ProductMapper productMapper;
+//
+//    @Test
+//    public void insertTest() {
+//        for(int i = 1; i <= 50; i++) {
+//            ProductVO vo = ProductVO.builder()
+//                    .prodEnddate("2024-08-15")
+//                    .prodWriter("admin")
+//                    .prodName("coding404" + i)
+//                    .prodPrice(i * 100)
+//                    .prodCount(i * 10)
+//                    .prodDiscount(i)
+//                    .prodPurchaseYn("N")
+//                    .prodContent("coding404" + i)
+//                    .prodComment("coding404" + i)
+//                    .build();
+//            productMapper.productInsert( vo );
+//        }
+//    }
+
+}
